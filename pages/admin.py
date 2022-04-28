58b163b9dbebde569a6cfa9dabeef3c4733625cf
Tiago Magnus <tiagomagnus2010@hotmail.com>
2022-04-28 00:06:04 -0300

    Registrando modelos e criando usuario admin por default para funcionalidade de administrador

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+models = apps.get_models()
+
+for model in models:
+  try:
+    admin.site.register(model)
+  except admin.sites.AlreadyRegistered:
+    pass
+
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+  user = User.objects.create_user('admin', 'admin@admin.com', 'admin', is_staff=True)
