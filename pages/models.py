163794e3f904cdf5fb6131a40a79dcad87ac6de1
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2022-05-02 19:48:42 -0300

    Add unique_together constraint for products

--- pages/models.py
+++ pages/models.py
@@ -15,6 +15,9 @@ class Product(models.Model):
     def __str__(self):
         return "%s : %s" % (self.title, self.description)
 
+    class Meta:
+        unique_together = ("title", "original_owner")
+
 
 class Trade(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
